c10184f22ed49d9917c274dcf903579dc21b42f0
giriputra <giriputra.b@gmail.com>
2021-03-25 18:29:29 +0700

    cleaned up and added comments

--- script.js
+++ script.js
@@ -30,11 +30,11 @@ function addToCart(namaDOM, quantity = 1) {
         }
     }
 }
-// Function 2: Update shopping cart jika user mengubah data. Misalkan quantitynya dikurangin atau ditambah.
+// Function 2: Update shopping cart jika user mengubah data. Misalkan quantitynya dikurangin atau ditambah. DONE
 
 function updateCartPlus(namaDOM) {
     for (const key of arrayShoppingCart) {
-        if (namaDOM===key.nama){
+        if (namaDOM === key.nama) {
             key.quantity++
         }
     }
@@ -42,32 +42,37 @@ function updateCartPlus(namaDOM) {
 
 function updateCartMinus(namaDOM) {
     for (const key of arrayShoppingCart) {
-        if (namaDOM===key.nama){
+        if (namaDOM === key.nama) {
             key.quantity--
         }
     }
 }
-// Function 3: Read arrayShoppingCart untu mengetahui sudah ada barang apa saja di shopping cart.
-// Function 4: Hapus data pada arrayShoppingCart jika user klik delete pada item tersebut.
 
+// Function 3: Read arrayShoppingCart untuk mengetahui sudah ada barang apa saja di shopping cart.
+function updateCart() {
+    // Gunakan Selector ke Class.
+    // Generate Cards menggunakan data dari variable {arrayShoppingCart}
+}
+
+// Function 4: Hapus data pada arrayShoppingCart jika user klik delete pada item tersebut. DONE
 function deleteCart(namaDOM) {
     for (let i = 0; i < arrayShoppingCart.length; i++) {
-        // delete arrayShoppingCart[i];
-        if(namaDOM===arrayShoppingCart[i].nama){
-            arrayShoppingCart.splice(i,1)
+        if (namaDOM === arrayShoppingCart[i].nama) {
+            arrayShoppingCart.splice(i, 1)
         }
-        
-        
     }
 }
 
 // Add To Cart mendapatkan argument dari DOM. Argument: Nama, Quantity
 addToCart('Cheesecake', 5)
 addToCart('Pudding', 5)
-// updateCartPlus('Cheesecake')
-// console.log(arrayShoppingCart)
-// updateCartMinus('Cheesecake')
-// console.log(arrayShoppingCart, 'after')
 
+//Update Cart Plus dan Minus menerima argument dari DOM. Argument: Nama
+updateCartPlus('Cheesecake')
+
+//Delete Cart menerima argument dari DOM. Argument: Nama
 deleteCart('Cheesecake')
-console.log(arrayShoppingCart, 'after')
+
+// TODO LIST:
+// 1. Generate Cards menggunakan data dari {arrayFoods}
+// 2. document.getElements
