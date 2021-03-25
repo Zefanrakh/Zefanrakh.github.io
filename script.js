cf758bb05382f149b3b01f7f33befd64f6a971c9
giriputra <giriputra.b@gmail.com>
2021-03-25 15:15:36 +0700

    first draft

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-// Test File
+// Stores Shopping Cart Data, Array of Object
+arrayShoppingCart = []
+
+// Stores All Food Object, Array of Object
+arrayFoods = []
+
+// Object dari makanan, kalo mau add makanan bisa pake format ini.
+foodObject = {
+    nama: '',
+    description: '',
+    harga: 0,
+    linkFoto: '',
+}
+
+// // Test Code
+// foodObject.nama = "Apple Pudding";
+// foodObject.description = "Pudding rasa apple.";
+// foodObject.harga = 25000;
+// foodObject.linkFoto = '/assets/bely6.jpg';
+// arrayFoods.push(foodObject);
+
+// foodObject.nama = "Manggo Pudding";
+// foodObject.description = "Pudding rasa Manggo.";
+// foodObject.harga = 35000;
+// foodObject.linkFoto = '/assets/bely6.jpg';
+// arrayFoods.push(foodObject);
+
+// console.log(arrayFoods);
